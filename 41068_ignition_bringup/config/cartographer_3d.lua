cd811764ad0f9463084cecd32c52f9fe47cf6e45
Juan Rodriguez <juan.rodriguez2593@gmail.com>
2025-10-29 03:26:51 +1100

    Added 41068_ignition_bringup package for Cartographer + Husky

---
+++ 41068_ignition_bringup/config/cartographer_3d.lua
@@ -0,0 +1,62 @@
+include "map_builder.lua"
+include "trajectory_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
+
+  map_frame = "map",
+  tracking_frame = "imu_link",
+  published_frame = "base_link",
+  odom_frame = "odom",
+  provide_odom_frame = true,
+  publish_frame_projected_to_2d = false,
+
+  use_odometry = true,
+  use_nav_sat = false,
+  use_landmarks = false,
+
+  num_laser_scans = 0,
+  num_multi_echo_laser_scans = 0,
+  num_subdivisions_per_laser_scan = 1,
+  num_point_clouds = 1,
+
+  -- required by your Cartographer build
+  rangefinder_sampling_ratio = 1.0,
+  odometry_sampling_ratio = 1.0,
+  fixed_frame_pose_sampling_ratio = 1.0,
+  imu_sampling_ratio = 1.0,
+  landmarks_sampling_ratio = 1.0,
+
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.5,
+  pose_publish_period_sec = 0.05,
+  trajectory_publish_period_sec = 0.05,
+}
+
+MAP_BUILDER.use_trajectory_builder_3d = true
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 3
+TRAJECTORY_BUILDER_3D.min_range = 0.3
+TRAJECTORY_BUILDER_3D.max_range = 20.0
+TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.05
+
+POSE_GRAPH.optimize_every_n_nodes = 120
+POSE_GRAPH.constraint_builder.min_score = 0.70
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.75
+
+options.tracking_frame = "imu_link"   -- you can switch to base_link later if IMU is noisy
+
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 3
+
+POSE_GRAPH.constraint_builder.min_score = 0.7
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.75
+
+TRAJECTORY_BUILDER_3D.motion_filter.max_time_seconds    = 0.8
+TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0.2
+TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians   = 0.02
+
+POSE_GRAPH.optimize_every_n_nodes = 120
+MAP_BUILDER.collate_by_trajectory = true
+
+
+return options
