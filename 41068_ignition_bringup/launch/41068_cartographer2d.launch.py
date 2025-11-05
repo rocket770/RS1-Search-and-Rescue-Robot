a2e69765504a82635170b328d79a136b63e5bfb0
juan5933 <157328481+juan5933@users.noreply.github.com>
2025-11-06 10:28:05 +1100

    Enhance comments in 41068_cartographer2d.launch.py
    Added comments to clarify the purpose of launch arguments and nodes.

--- 41068_ignition_bringup/launch/41068_cartographer2d.launch.py
+++ 41068_ignition_bringup/launch/41068_cartographer2d.launch.py
@@ -6,9 +6,9 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='True')
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='True') #Expose use_sim_time.
 
-    cfg_dir = PathJoinSubstitution([FindPackageShare('41068_ignition_bringup'), 'config'])
+    cfg_dir = PathJoinSubstitution([FindPackageShare('41068_ignition_bringup'), 'config']) #Config directory where Lua files live.
 
     carto = Node(
         package='cartographer_ros',
@@ -25,7 +25,7 @@ def generate_launch_description():
             ('odom', '/odometry'),
             ('imu',  '/imu'),
         ],
-    )
+    )    #start cartographer ROS node; pass lua file path; remap i)nput topics to robots topics; publish map-odom/submap-list
 
     occ = Node(
         package='cartographer_ros',
@@ -38,6 +38,6 @@ def generate_launch_description():
             ('map', '/map'),
             ('map_updates', '/map_updates'),
         ],
-    )
+    )        #Converts Cartographer submaps into a rolling 2D occupancy grid for RViz/Nav2 (/map, /map_updates), at 5 cm resolution.
 
     return LaunchDescription([use_sim_time_arg, carto, occ])
